cc72728669a7d250c9485e02aaf0a7d8e3e0abd8
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-09-13 11:22:50 -0400

    responsive styling to figures

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,18 +10,20 @@ const Home = () => {
   };
 
   return (
-    <div className=" bg-white gap-6 md:columns-2 sm:columns-1 flex">
+    <div className=" bg-white gap-6 flex lg:flex-row flex-col">
       <div className="flex-col items-center py-20 px-10">
         {/* <img className="w-50 h-50" src="https://i.imgur.com/1zv3Z9I.jpg" alt="camera" /> */}
-        <h1 className="font-display text-3xl pb-5">
+        <h1 className="font-display md:text-6xl text-4xl pb-5">
           Riley Addison Photography
         </h1>
-        <p className="inline-flex">Capturing every special moment</p>
-        <p className="inline-flex pb-4">I love photography</p>
+        <p className="md:text-3xl text-2xl inline-flex">
+          Capturing every special moment
+        </p>
+        <p className="text-1xl flex pb-4">I love photography</p>
         <button
           onClick={handleClick}
           type="button"
-          className="block bg-bblue p-1 px-3 text-s font-medium text-white "
+          className="text-1xl block bg-bblue p-1 px-3 font-medium text-white "
         >
           see my portfolio
         </button>
@@ -30,114 +32,83 @@ const Home = () => {
       {/* portfolio cards */}
       <div className="flex min-h-[140px] w-full p-4">
         {/* First card */}
-        <figure className="relative w-1/3 h-full flex-shrink-0 p-1">
+        <figure className="relative w-1/3 h-full flex p-1">
           <img
-            className="object-cover object-center w-full h-full"
+            className="object-cover object-center w-full h-full flex-shrink"
             src="https://cindyswansonphotography.com/wp-content/uploads/2019/01/10-8374-post/senior_portraits_in_dallas_for_guys_3.jpg"
             alt="senior portrait"
           />
-          <figcaption className="absolute bottom-4 inset-x-0 mx-4 flex flex-col md:flex-row justify-between items-center rounded-xl border border-white bg-white/75 p-4 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm">
+          <figcaption className="absolute bottom-4 inset-x-0 mx-4 flex-col lg:flex-row justify-between items-center rounded-xl border border-white bg-white/75 p-3 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm flex">
             <div className="text-center md:text-left">
-              <h5 className="font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900">
+              <h5 className="font-sans lg:text-xl text-base font-semibold leading-snug tracking-normal text-blue-gray-900 hidden sm:flex">
                 Senior Portrait
               </h5>
-              <p className="mt-1 font-sans text-base font-normal leading-relaxed text-gray-700">
+              <p className="mt-0 font-sans md:text-base text-sm font-normal leading-relaxed text-gray-700 hidden sm:flex">
                 James Craig
               </p>
+              <button
+                onClick={handleClick}
+                type="button"
+                className="bg-white mt-1 py-1 px-3 md:text-base text-sm font-medium text-black rounded"
+              >
+                see more
+              </button>
             </div>
-            <button
-              onClick={handleClick}
-              type="button"
-              className="bg-poppy p-2 px-4 text-base font-medium text-white rounded"
-            >
-              see more
-            </button>
           </figcaption>
         </figure>
 
         {/* Second card */}
-        <figure className="relative w-1/3 h-full flex-shrink-0 p-1">
+        <figure className="relative w-1/3 h-full flex p-1">
           <img
-            className="object-cover object-center w-full h-full"
+            className="object-cover object-center w-full h-full flex-shrink"
             src="https://www.mintarrow.com/wp-content/uploads/2021/12/RESIZED-Family-04278-1097x1536.jpg"
             alt="family portrait"
           />
-          <figcaption className="absolute bottom-4 inset-x-0 mx-4 flex flex-col md:flex-row justify-between items-center rounded-xl border border-white bg-white/75 p-4 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm">
+          <figcaption className="absolute bottom-4 inset-x-0 mx-4 flex-col lg:flex-row justify-between items-center rounded-xl border border-white bg-white/75 p-3 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm flex">
             <div className="text-center md:text-left">
-              <h5 className="font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900">
+              <h5 className="font-sans lg:text-xl text-base font-semibold leading-snug tracking-normal text-blue-gray-900 hidden sm:flex">
                 Family Portrait
               </h5>
-              <p className="mt-1 font-sans text-base font-normal leading-relaxed text-gray-700">
+              <p className="mt-0 font-sans md:text-base text-sm font-normal leading-relaxed text-gray-700 hidden sm:flex">
                 Johnson Family
               </p>
-            </div>
-            <p className="flex items-center mt-2 md:mt-0">
               <button
                 onClick={handleClick}
                 type="button"
-                className="bg-poppy p-2 px-4 text-base font-medium text-white rounded"
+                className="bg-white mt-1 py-1 px-3 md:text-base text-sm font-medium text-black rounded"
               >
                 see more
               </button>
-            </p>
+            </div>
           </figcaption>
         </figure>
 
         {/* Third card */}
-        <figure className="relative w-1/3 h-full flex-shrink-0 p-1">
+        <figure className="relative w-1/3 h-full flex p-1">
           <img
-            className="object-cover object-center w-full h-full"
+            className="object-cover object-center w-full h-full flex-shrink"
             src="https://th.bing.com/th/id/R.b3a33c7d7b01415e22e1ac1a010009eb?rik=dRK0X0qJaNfiBQ&pid=ImgRaw&r=0"
             alt="senior portrait"
           />
-          <figcaption className="absolute bottom-4 inset-x-0 mx-4 flex flex-col md:flex-row justify-between items-center rounded-xl border border-white bg-white/75 p-4 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm">
+          <figcaption className="absolute bottom-4 inset-x-0 mx-4 flex-col lg:flex-row justify-between items-center rounded-xl border border-white bg-white/75 p-3 shadow-lg shadow-black/5 saturate-200 backdrop-blur-sm flex">
             <div className="text-center md:text-left">
-              <h5 className="font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900">
+              <h5 className="font-sans lg:text-xl text-base font-semibold leading-snug tracking-normal text-blue-gray-900 hidden sm:flex">
                 Senior Portrait
               </h5>
-              <p className="mt-1 font-sans text-base font-normal leading-relaxed text-gray-700">
+              <p className="mt-0 font-sans md:text-base text-sm font-normal leading-relaxed text-gray-700 hidden sm:flex">
                 Alex Bush
               </p>
-            </div>
-            <p className="flex items-center mt-2 md:mt-0">
               <button
                 onClick={handleClick}
                 type="button"
-                className="bg-poppy p-2 px-4 text-base font-medium text-white rounded"
+                className="bg-white mt-1 py-1 px-3 md:text-base text-sm font-medium text-black rounded"
               >
                 see more
               </button>
-            </p>
+            </div>
           </figcaption>
         </figure>
       </div>
-
-      {/*  <div className="w-full h-full flex flex-col text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-black sm:shrink-0 sm:grow sm:basis-0 sm:rounded-s-none">
-          <a href="#!">
-            <img
-              className="p-1"
-              src="https://www.tumblestonphotography.com/wp-content/uploads/2018/05/high-school-senior-picture-session.jpg"
-              alt="Los Angeles Skyscrapers"
-            />
-          </a>
-          <div className="pt-1 pl-4">
-            <h5 className="mb-0 text-xl font-medium leading-tight">
-              Kyya Smith
-            </h5>
-            <p className="mb-4 text-base">
-              Senior Portraits
-            </p>
-            <p className="mb-4 text-base text-surface/75 dark:text-neutral-300">
-              <button
-                onClick={handleClick}
-                type="button"
-                className="bg-bblue p-1 px-3 text-s font-medium text-white"
-              >
-                see more
-              </button>
-            </p>
-          </div>
-        </div> */}
     </div>
   );
 };
