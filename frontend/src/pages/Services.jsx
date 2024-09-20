830eed0d4a04250b20778b0ed4861b86d03eb9a9
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-09-20 12:09:49 -0400

    component styling

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -3,12 +3,18 @@ import ServiceCards from "../Components/ServiceCards";
 import ServiceAccordion from "../Components/ServiceAccordion";
 
 const Services = () => {
+
   return (
     <div className="w-full h-100">
-      
+      <div>
       <ServiceCards />
+      </div>
+      <div className="bg-bblue flex flex-row">
+      <h1 className="basis-2/5"> WHAT'S NEXT? </h1>
+      <div className="basis-3/5 p-10">
       <ServiceAccordion />
-      
+      </div>
+      </div>
     </div>
   );
 };
