f6182c865e1f342968eca28cb6597732b1b26553
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-10-11 13:16:45 -0400

    styling

--- frontend/src/Components/AboutMe.jsx
+++ frontend/src/Components/AboutMe.jsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
       ></img> */}
       <div className='w-full h-screen relative flex flex-col justify-center items-center'>
         <img src="https://images.unsplash.com/photo-1541654056076-0a252e083078?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="w-full h-full object-cover absolute"/>
-        <div className="absolute right-36">
+        <div className="absolute right-96">
         <h1>Hi, I'm Riley</h1>
         <h2>
           You're go-to{" "}
