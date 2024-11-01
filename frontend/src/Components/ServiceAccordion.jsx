65ca4308a15347608d2c29eefc893f2fd03c71dd
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-11-01 17:28:10 -0400

    responsiveness

--- frontend/src/Components/ServiceAccordion.jsx
+++ frontend/src/Components/ServiceAccordion.jsx
@@ -11,7 +11,7 @@ const ServiceAccordion = () => {
   return (
     <div>
       <div>
-        <h2 className="sm:text-4xl text-black font-title ml-10">
+        <h2 className="lg:text-4xl sm:text-xl text-black font-title ml-10">
           Everything you need to know (FAQs):
         </h2>
       </div>
@@ -22,7 +22,7 @@ const ServiceAccordion = () => {
             01. Send Me a Request
           </AccordionTitle>
           <AccordionContent>
-            <p className="text-black font-body uppercase mb-4">
+            <p className="text-black sm:text-sm lg:text-base font-body uppercase mb-4">
               One you've decided which package you want, go to the&nbsp;
               <a
                 href="/Contact"
@@ -32,7 +32,7 @@ const ServiceAccordion = () => {
               </a>
               page and fill out the request form
             </p>
-            <p className="text-black font-body uppercase mt-2">
+            <p className="text-black font-body uppercase mt-2 sm:text-sm lg:text-base">
               I will reach out to you within 24 hours to schedule your session!
             </p>
           </AccordionContent>
@@ -44,11 +44,11 @@ const ServiceAccordion = () => {
             02. Consultation
           </AccordionTitle>
           <AccordionContent>
-            <p className="mb-4 text-black font-body uppercase pt-2">
+            <p className="mb-4 text-black font-body uppercase pt-2 sm:text-sm lg:text-base">
               Once we've scheduled our session, we'll have a consultation to
               discuss the details such as
             </p>
-            <ul className="list-disc pl-5 text-black font-body">
+            <ul className="list-disc pl-5 text-black font-body sm:text-sm lg:text-base">
               <li>Location </li>
               <li>Photo Inspiration/ Mood Board</li>
               <li>Styling Recommendations (if requested)</li>
@@ -71,15 +71,15 @@ const ServiceAccordion = () => {
             03. Payment
           </AccordionTitle>
           <AccordionContent>
-            <p className="mb-4 text-black font-body uppercase">
+            <p className="mb-4 text-black font-body uppercase sm:text-sm lg:text-base">
               At the end of our scheduling/ consultation communications, I'll
               send you an invoice for 50% of the session fee via text or e-mail.
               The remaining balance is due immediately following our session
             </p>
-            <p className="text-black font-body">
+            <p className="text-black font-body sm:text-sm lg:text-base">
               I take the following forms of payments:
             </p>
-            <ul className="list-disc pl-5 text-black font-body">
+            <ul className="list-disc pl-5 text-black font-body sm:text-sm lg:text-base">
               <li>Cash </li>
               <li>Venmo</li>
               <li>Zelle</li>
@@ -93,14 +93,14 @@ const ServiceAccordion = () => {
             04. Sneak Peeks & Gallery Access
           </AccordionTitle>
           <AccordionContent>
-            <p className="mb-4 text-black font-body uppercase">
+            <p className="mb-4 text-black font-body uppercase sm:text-sm lg:text-base">
               Sneak peeks will be delivered within{" "}
               <span className="font-bold">48</span> hours of our session via
               text as a Google Drive link. The full gallery will be delivered
               within <span className="font-bold">7</span> days of our session
               via email with an updated link.
             </p>
-            <p className="mb-2 text-black font-body">
+            <p className="mb-2 text-black font-body sm:text-sm lg:text-base">
               <span className="font-bold">Note: </span> You do{" "}
               <span className="font-bold">NOT</span> need a Google account to
               view your gallery
