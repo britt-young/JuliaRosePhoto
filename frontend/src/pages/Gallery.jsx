06b68a4f1bbff9f0329d9adfb52173b65231cdcf
Britt Young <140761880+britt-young@users.noreply.github.com>
2024-11-01 17:45:09 -0400

    CTA added

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import GalleryMix from '../Components/GalleryMix'
+import GalleryBanner from '../Components/GalleryBanner'
 
 const Gallery = () => {
   return (
     <div className="w-full h-max p-10">
       <GalleryMix />
+      <GalleryBanner />
     </div>
   )
 }
