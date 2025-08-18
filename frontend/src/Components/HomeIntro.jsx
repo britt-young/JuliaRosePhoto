6858e8c2b74b0bb20d63fe93beb7d5af4ddb9f8e
Britt Young <140761880+britt-young@users.noreply.github.com>
2025-08-18 16:31:30 -0400

    header video replaced and styling changes

--- frontend/src/Components/HomeIntro.jsx
+++ frontend/src/Components/HomeIntro.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
-// import bgImage from "../assets/img/1.png";
-import vid from "../assets/vid/alt-video.mp4";
+import bgImage from "../assets/img/headerimg.jpg";
+// import vid from "../assets/vid/alt-video.mp4";
 
 const HomeIntro = () => {
   const navigate = useNavigate();
@@ -12,30 +11,27 @@ const HomeIntro = () => {
   };
 
   return (
-    <div className="relative h-screen mx-auto flex flex-col items-center justify-center text-center bg-cover bg-no-repeat bg-center">
-    {/* style={{ backgroundImage: `url(${bgImage})` }}> */}
+    <div
+      className="relative min-h-[800px] bg-cover bg-no-repeat bg-center flex flex-col text-center items-center font-serif"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       {/* Video background */}
-       <video
+      {/* <video
         src={vid}
         type="video/mp4"
         autoPlay
         loop
         muted
         className="absolute top-0 left-0 w-full h-full object-cover"
-      />
-      
-      <h1 className="flex flex-col items-center font-title sm:text-5xl lg:text-7xl mb-5 z-10">
-        Julia Rose Photography
-      </h1>
-      <div className="max-w-7xl mx-auto flex flex-col items-center justify-center text-center z-10">
-        <p className="font-body lg:text-md pb-4 uppercase mx-5 lg:mx-0">
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, ratione."
+      /> */}
+
+      <div className="mx-5 mt-40">
+        <h1 className="text-3xl lg:text-5xl lg:mb-5 ">Julia Rose Photography</h1>
+        <p className="lg:text-md pb-4 uppercase">
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla,
+          ratione."
         </p>
-        <button
-          onClick={handleClick}
-        >
-          Book with me
-        </button>
+        <button onClick={handleClick}>Book with me</button>
       </div>
     </div>
   );
